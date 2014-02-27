bcb7ec590d5e74a9d51ecd22733312f9413d3a17
Kuo Hern Wong <Kuohern@dyn1219-110.wlan.ic.ac.uk>
2014-02-27 16:24:20 +0000

    fml

--- my_wol.pl
+++ my_wol.pl
@@ -168,7 +168,8 @@ look_down_two_ply('b', [Move | PossMoves], [Blue, Red], BestMove, MoveAdv, Ultim
   generate_next_state('b', Move, [Blue, Red], [TempBlue, TempRed]),
   ((\+length(TempBlue, 0), \+length(TempRed, 0)) -> (land_grab('r', [TempBlue, TempRed], NextBoardState, _),
                               next_generation(NextBoardState, [CrankBlue, CrankRed]),
-                              ((\+length(CrankBlue, 0); \+length(CrankRed,0)) -> (land_grab('b', [CrankBlue, CrankRed], FinalBoardState, _),
+                              ((\+length(CrankBlue, 0,); \+length(CrankRed,0)) -> (land_grab('b', [CrankBlue, CrankRed], FinalBoardState, _),
+                                                          write(fuck), nl,
                                                           next_generation(FinalBoardState, [FinalBlue, FinalRed]));
                                                           (FinalBlue = CrankBlue, FinalRed = CrankRed))); 
                               (FinalBlue = Blue, FinalRed = Red)),
@@ -183,6 +184,7 @@ look_down_two_ply('r', [Move | PossMoves], [Blue, Red], BestMove, MoveAdv, Ultim
   ((\+length(TempBlue, 0), \+length(TempRed, 0)) -> (land_grab('b', [TempBlue, TempRed], NextBoardState, _),
                               next_generation(NextBoardState, [CrankBlue, CrankRed]),
                               ((\+length(CrankBlue, 0); \+length(CrankRed,0)) -> (land_grab('r', [CrankBlue, CrankRed], FinalBoardState, _),
+                                                          write(fuck), nl,
                                                           next_generation(FinalBoardState, [FinalBlue, FinalRed]));
                                                           (FinalBlue = CrankBlue, FinalRed = CrankRed))); 
                               (FinalBlue = Blue, FinalRed = Red)),
